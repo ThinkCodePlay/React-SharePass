4961d205f22fd7b52c6216072e8b2de75a2d18a1
Yonatan Kravitz <yonatank@kan.org.il>
2022-06-07 12:39:28 +0300

    fixed bootsrap collapse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,35 +2,33 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <Link to={"/"} className="navbar-brand">
-          SharePass
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <NavLink to={"/encrypt"} className="nav-link">
+    <div className="">
+           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand">
+            SharePass
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <NavLink to={"/encrypt"} className="nav-item nav-link">
                 Encrypt
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to={"/decrypt"} className="nav-link">
+              <NavLink to={"/decrypt"} className="nav-item nav-link">
                 Dycrypt
               </NavLink>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
